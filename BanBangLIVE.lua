7231d458b542a68554119a28fab1e2fb8bef7d20
LILOBONdev <naly87267@gmail.com>
2024-07-03 02:51:35 +0500

    Create BanBangLIVE.lua

---
+++ BanBangLIVE.lua
@@ -0,0 +1,18 @@
+local TargetUserId = {
+	'6001782755',
+}
+local plr = game:GetService("Players").LocalPlayer
+local ToId = tonumber(plr.UserId)
+local Out = plr.AccountAge + 1
+local function Kick()
+	plr:Kick("CHEAT LANG™")
+end
+
+for _, v in pairs(TargetUserId) do
+	local To = tonumber(v)
+	if To == ToId and plr.AccountAge < Out then
+		Kick()
+	elseif plr.AccountAge > Out then
+		print("CHEAT LANG™ - The lockout time has expired, continue playing!")
+	end
+end
