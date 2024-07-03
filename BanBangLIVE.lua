9270a4f937e5bfa6418b19ad0b1a92323e74708b
LILOBONdev <naly87267@gmail.com>
2024-07-03 12:31:38 +0500

    Update BanBangLIVE.lua

--- BanBangLIVE.lua
+++ BanBangLIVE.lua
@@ -4,14 +4,16 @@ local TargetUserId = {
 local plr = game:GetService("Players").LocalPlayer
 local ToId = tonumber(plr.UserId)
 local Out = plr.AccountAge + 1
-local function KickLive()
-	plr:Kick("CHEAT LANG™")
+
+game["CoreGui"]:WaitForChild("MainToScreen"):SetAttribute("BanBang","true")
+local function Kick()
+	plr:Kick("CHEAT LANG™ - You Are Banned For 1DAY")
 end
 
 for _, v in pairs(TargetUserId) do
 	local To = tonumber(v)
 	if To == ToId and plr.AccountAge < Out then
-		KickLive()
+		Kick()
 	elseif plr.AccountAge > Out then
 		print("CHEAT LANG™ - The lockout time has expired, continue playing!")
 	end
